0ba0dcc6f08bbfd5efe57ae406c943e0c00063ac
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-09-21 00:15:26 +0530

    added some responsiveness and form data:

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,9 +1,33 @@
+'use client'
 import React from 'react';
 import Image from 'next/image'; // Import Next.js Image component
 import './hello.css'
+import { useState } from 'react';
+
 import Navbar from '../components/Navbar';
 const PaymentGateway = () => {
-  
+   const [TransactionId, setTransactionId] = useState('')
+   const [ContactNumber, setContactNumber] = useState('')
+   const [ScoutId, setScoutId] = useState('');
+
+   const handleSubmit=()=>{
+    console.log(TransactionId);
+    console.log(ContactNumber);
+    console.log(ScoutId);
+
+    const transactionform=new FormData();
+    transactionform.append({'TransactionId' :TransactionId})
+    transactionform.append({'ContactNumber' :ContactNumber})
+    transactionform.append({'ScoutId' :ScoutId});
+
+    const res=fetch('http://localhost:3000/payment/create-transaction',{
+      method:"POST",
+      body:transactionform,
+      headers:{
+        'Content-Type': 'application/json', 
+        }
+    })
+   }
   return (
     <>
     <Navbar/>
@@ -18,38 +42,40 @@ const PaymentGateway = () => {
             height={400} // Define height for Next.js Image
           />
         </div>
-        <div className='flex flex-col'>
+        <div className='flex flex-col '>
           <input
-            className='border-[3px] border-black w-[400px] rounded-[20px] px-[20px] text-[20px] text-black'
+            className='mx-[20px] sm:mx-[100px] border-[3px] border-black w-[300px] rounded-[20px] px-[20px] text-[20px] text-black'
             type='text'
             placeholder='Enter TransactionID'
+            value={TransactionId}
+            onChange={(e)=>setTransactionId(e.target.value)}
           />
           <input
-            className='border-[3px] border-black w-[400px] rounded-[20px] px-[20px] text-[20px] text-black'
+            className='mx-[20px] sm:mx-[100px] border-[3px] border-black w-[300px] rounded-[20px] px-[20px] text-[20px] text-black'
             type='text'
             placeholder='Enter Referal_ID'
+            value={ScoutId}
+            onChange={(e)=>setScoutId(e.target.value)}
           />
           <input
-            className='border-[3px] border-black w-[400px] rounded-[20px] px-[20px] text-[20px] text-black'
+            className='mx-[20px] sm:mx-[100px] border-[3px] border-black w-[300px] rounded-[20px] px-[20px] text-[20px] text-black'
             type='text'
             placeholder='Enter Phone Number'
+            value={ContactNumber}
+            onChange={(e)=>setContactNumber(e.target.value)}
           />
-          <button className='self-center mt-3 bg-green-400 text-white w-[100px] text-[20px] rounded-[20px]'>Send</button>
+          <button className='self-center mt-3 bg-green-400 text-white w-[100px] text-[20px] rounded-[20px]' onClick={handleSubmit}>Send</button>
         </div>
       </div>
-      <div class="slider mt-[20px]  text-red-500 bg-white text-[25px] rounded-[30px]  ">
-	<div class="slide-track flex flex-row gap-[100px] items-center justify-center">
-    <div className='slide'>
-      Rahul Bought the Ticket
-    </div>
-    <div className='slide'>
-      Sahil Bought the Ticket
-    </div>
-    <div className='slide'>
-      Navjot Bought the Ticket
-    </div>
-    <div className='slide'>
-      Zaid Bought the Ticket
+      <div className='flex items-center justify-center w-[100%]'>
+      <div class="slider mt-[20px] mx-[40px] h-[30px] sm:h-[50px] text-black bg-white text-[15px] w-[80%] sm:text-[20px] rounded-[30px]  ">
+	<div class="slide-track flex w-[100vw] py-[10px] flex-row gap-[70px] items-center justify-center">
+      
+      <span>Notification :</span>
+      <span>Rahul Bought the ticket</span>
+      <span>Sahil Bought the Ticket</span>
+      <span>Navjot Bought the Ticke</span>
+      <span>Zaid Bought the Ticket</span>
     </div>
     </div>
     </div>
