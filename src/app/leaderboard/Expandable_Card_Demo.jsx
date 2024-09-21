350f541b4cfdce5ac193073f9b2dba2e100a43bb
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-09-21 14:57:55 +0530

    added leaderboard etc

--- src/app/leaderboard/Expandable_Card_Demo.jsx
+++ src/app/leaderboard/Expandable_Card_Demo.jsx
@@ -119,9 +119,9 @@ export function ExpandableCardDemo() {
         <motion.div
           layoutId={`card-${card.title}-${id}`}
           key={`card-${card.title}-${id}`}
-          onClick={() => setActive(card)}
           className="p-4 flex flex-col md:flex-row justify-between items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer">
           <div className="flex gap-4 flex-col md:flex-row ">
+          {index+4}
             <motion.div layoutId={`image-${card.title}-${id}`}>
               <Image
                 width={100}
@@ -146,7 +146,7 @@ export function ExpandableCardDemo() {
           <motion.button
             layoutId={`button-${card.title}-${id}`}
             className="px-4 py-2 text-sm rounded-full font-bold bg-gray-100 hover:bg-green-500 hover:text-white text-black mt-4 md:mt-0">
-            {card.ctaText}
+            {card.Referraldone}
           </motion.button>
         </motion.div>
       ))}
@@ -154,306 +154,81 @@ export function ExpandableCardDemo() {
   </>);
 }
 
-export const CloseIcon = () => {
-  return (
-    (<motion.svg
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      exit={{
-        opacity: 0,
-        transition: {
-          duration: 0.05,
-        },
-      }}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-4 w-4 text-black">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M18 6l-12 12" />
-      <path d="M6 6l12 12" />
-    </motion.svg>)
-  );
-};
-
 const cards = [
   {
     description: "Lana Del Rey",
     title: "Rahul Singh",
     src: "https://assets.aceternity.com/demos/lana-del-rey.jpeg",
-    ctaText: "14",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-                    her melancholic and cinematic music style. Born Elizabeth Woolridge
-                    Grant in New York City, she has captivated audiences worldwide with
-                    her haunting voice and introspective lyrics. <br /> <br />Her songs
-                    often explore themes of tragic romance, glamour, and melancholia,
-                    drawing inspiration from both contemporary and vintage pop culture.
-                    With a career that has seen numerous critically acclaimed albums, Lana
-                    Del Rey has established herself as a unique and influential figure in
-                    the music industry, earning a dedicated fan base and numerous
-                    accolades.
-                  </p>)
-      );
+    Referraldone: "14"
     },
-  },
   {
     description: "Babbu Maan",
     title: "Kunal Dubey",
     src: "https://assets.aceternity.com/demos/babbu-maan.jpeg",
-    ctaText: "12",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Babu Maan, a legendary Punjabi singer, is renowned for his soulful
-                    voice and profound lyrics that resonate deeply with his audience. Born
-                    in the village of Khant Maanpur in Punjab, India, he has become a
-                    cultural icon in the Punjabi music industry. <br /> <br />His songs
-                    often reflect the struggles and triumphs of everyday life, capturing
-                    the essence of Punjabi culture and traditions. With a career spanning
-                    over two decades, Babu Maan has released numerous hit albums and
-                    singles that have garnered him a massive fan following both in India
-                    and abroad.
-                  </p>)
-      );
+    Referraldone: "12",
     },
-  },
-
   {
     description: "Metallica",
     title: "For Whom The Bell Tolls",
     src: "https://assets.aceternity.com/demos/metallica.jpeg",
-    ctaText: "10",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        <></>
-      );
-    },
+    Referraldone: "10",
   },
   {
-    description: "Led Zeppelin",
-    title: "Stairway To Heaven",
+    description: "Andrew Tate",
+    title: "Andrew Tate",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "8",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
+    Referraldone: "8",
   },
   {
-    description: "Led Zeppelin",
-    title: "Stairway To Heaven",
+    description: "Stephen Hawkings",
+    title: "Stephen Hawkings",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "8",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
+    Referraldone: "8",
+  }, {
+    description: "Chaman Kajaria",
+    title: "Chaman Kajaria",
+    src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
+    Referraldone:"8",
+  },{
+    description: "Nikhil Singh",
+    title: "Nikhil Singh",
+    src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
+    Referraldone: "8",
+  }, {
+    description: "Himanshu gupta",
+    title: "Himanshu gupta",
+    src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
+    Referraldone: "8",
   }, {
     description: "Led Zeppelin",
     title: "Stairway To Heaven",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "8",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
+    Referraldone:"8",
   }, {
     description: "Led Zeppelin",
     title: "Stairway To Heaven",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "8",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
+    Referraldone:"4",
   }, {
     description: "Led Zeppelin",
     title: "Stairway To Heaven",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "8",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
+    Referraldone:"2",
   }, {
     description: "Led Zeppelin",
     title: "Stairway To Heaven",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "8",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
+    Referraldone:"2",
   }, {
     description: "Led Zeppelin",
     title: "Stairway To Heaven",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "4",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
-  }, {
-    description: "Led Zeppelin",
-    title: "Stairway To Heaven",
-    src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "2",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
-  }, {
-    description: "Led Zeppelin",
-    title: "Stairway To Heaven",
-    src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "2",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
-  }, {
-    description: "Led Zeppelin",
-    title: "Stairway To Heaven",
-    src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "1",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>Led Zeppelin, a legendary British rock band, is renowned for their
-                    innovative sound and profound impact on the music industry. Formed in
-                    London in 1968, they have become a cultural icon in the rock music
-                    world. <br /> <br />Their songs often reflect a blend of blues, hard
-                    rock, and folk music, capturing the essence of the 1970s rock era.
-                    With a career spanning over a decade, Led Zeppelin has released
-                    numerous hit albums and singles that have garnered them a massive fan
-                    following both in the United Kingdom and abroad.
-                  </p>)
-      );
-    },
+    Referraldone:"1",
   },
   {
     description: "Mustafa Zahid",
     title: "Toh Phir Aao",
     src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
-    ctaText: "5",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        (<p>"Aawarapan", a Bollywood movie starring Emraan Hashmi, is
-                    renowned for its intense storyline and powerful performances. Directed
-                    by Mohit Suri, the film has become a significant work in the Indian
-                    film industry. <br /> <br />The movie explores themes of love,
-                    redemption, and sacrifice, capturing the essence of human emotions and
-                    relationships. With a gripping narrative and memorable music,
-                    "Aawarapan" has garnered a massive fan following both in
-                    India and abroad, solidifying Emraan Hashmi's status as a
-                    versatile actor.
-                  </p>)
-      );
-    },
+    Referraldone:"0",
   },
 ];
