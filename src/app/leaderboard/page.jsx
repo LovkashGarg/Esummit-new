f66437d2d9765f58c08694ac01b1c137fc8842fa
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-09-26 11:34:01 +0530

    made loader

--- src/app/leaderboard/page.jsx
+++ src/app/leaderboard/page.jsx
@@ -1,14 +1,33 @@
+'use client'
 import React from 'react'
 import Leaderboard from '../components/LeaderBoard'
 import { ExpandableCardDemo } from './Expandable_Card_Demo'
 import Navbar from '../components/Navbar'
+import { useState,useEffect } from 'react'
+import InfinityLoader from '../components/infinite_loader'
 const page = () => {
+  const [loading, setLoading] = useState(true);
+
+  // Simulate a data fetch with a timeout
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Set loading to false after 2 seconds
+    }, 2000);
+
+    return () => clearTimeout(timer); // Clean up the timeout
+  }, []);
   return (
+    <div>
+    {loading ? (
+      <InfinityLoader /> // Show loader while loading
+    ) : (
     <>
     <Navbar/>
 <Leaderboard/>
 <ExpandableCardDemo/>
     </>
+    )}
+    </div>
   )
 }
 
