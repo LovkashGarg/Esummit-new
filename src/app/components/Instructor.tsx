bbdfa991297aa3245caec7595cf73c5d7858393d
garv190 <garv.jauhari@gmail.com>
2024-09-27 12:08:37 +0530

    instructor

--- src/app/components/Instructor.tsx
+++ src/app/components/Instructor.tsx
@@ -9,49 +9,49 @@ const instructors = [
       name: 'Satyam Garg',
       // designation: 'Vocal Coach',
       image:
-        '/sanskar.jpg',
+        '/satyam.jpg',
     },
     {
       id: 2,
       name: 'Akshat Chaturvedi',
       // designation: 'Guitar Instructor',
       image:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80',
+        '/arpan.jpg',
     },
     {
       id: 3,
       name: 'Arpan Gupta',
       // designation: 'Piano Teacher',
       image:
-        'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+        '/arpan.jpg',
     },
     {
       id: 4,
       name: 'Sannidhya Walzade',
       // designation: 'Drumming Expert',
       image:
-        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+        '/sannidhya.jpg',
     },
     {
       id: 5,
       name: 'Sanskar Hasija',
       // designation: 'Drumming Expert',
       image:
-        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+        '/sanskar.jpg',
     },
     {
       id: 6,
       name: 'Riya Aggarwal',
       // designation: 'Drumming Expert',
       image:
-        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+        '/riya.jpg',
     },
     {
       id: 7,
       name: 'Sarthak Jain',
       // designation: 'Drumming Expert',
       image:
-        'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+      '/sarthak.jpg',
     },
   ];
 
