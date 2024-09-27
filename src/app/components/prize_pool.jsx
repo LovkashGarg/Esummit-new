29c14dedb9383c5e02c07221029ae688972e5f6e
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-09-28 00:28:42 +0530

    added speaker and prize pool with card settled gap

--- src/app/components/prize_pool.jsx
+++ src/app/components/prize_pool.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function PrizePool() {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-900 rounded-lg p-6 shadow-lg max-w-md mx-auto">
+    <div className="flex flex-col items-center justify-center bg-slate-900 rounded-lg p-6 shadow-lg max-w-md mx-auto">
       <h2 className="text-3xl font-bold text-white mb-2">Prize Pool</h2>
       <div className="flex items-center justify-center">
         <span className="text-6xl font-extrabold text-yellow-400">₹80K</span>
--- src/app/components/ui/card-hover-effect.tsx
+++ src/app/components/ui/card-hover-effect.tsx
@@ -26,7 +26,7 @@ export const HoverEffect = ({
      <div className=" text-[40px] sm:text-[60px] pt-[50px] text-">Our Events</div>
     <div
       className={cn(
-        "h-[350vh] sm:h-[150vh] md:h-[70vh] grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  ",
+        "h-[300vh] sm:h-[150vh] md:h-[80vh] grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  ",
         className
       )}
     >
