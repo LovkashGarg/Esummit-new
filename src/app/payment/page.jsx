a0f8a64aacd19e7c0d6d34f87df1af5840e7adf7
garv190 <garv.jauhari@gmail.com>
2024-09-28 17:08:33 +0530

    commit

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -34,15 +34,15 @@ const TicketSection = () => {
   ];
 
   return (
-    <>
+    <div class="bg-black">
     <Navbar/>
     <div className="mt-[200px] sm:mt-[100px] max-w-7xl mx-auto py-12">
-      <h2 className="text-4xl font-bold text-center mb-12 ">Choose Your Tickets</h2>
+      <h2 className="text-4xl font-bold text-center mb-12 text-white ">Choose Your Tickets</h2>
       
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {tickets.map(ticket => (
            <div className='flex mx-[10%]  flex-col'>
-           <div key={ticket.id} className={`${ticket.id==2? 'block':'hidden'} bg-slate-900 text-center h-[40px] text-[30px] rounded-[10px] `}>Most Popular</div>
+           <div key={ticket.id} className={`${ticket.id==2? 'block':'hidden'} bg-slate-900 text-center h-[40px] text-[30px] rounded-[10px]  text-white`}>Most Popular</div>
           <div key={ticket.id}  style={{ backgroundColor: ticket.color }}  className={` border-[5px] border-gray-300 rounded-[30px] p-6 mt-[10px] shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center`}>
             <h3 className="text-xl text-yellow-400 font-semibold mb-4">{ticket.title}</h3>
             <p className="text-white mb-4">{ticket.description}</p>
@@ -60,7 +60,8 @@ const TicketSection = () => {
       </div>
     </div>
     <Footer/>
-    </>
+    </div>
+ 
   );
 };
 
