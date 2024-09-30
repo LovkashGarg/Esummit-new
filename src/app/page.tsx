dcc0a103b29e182c2d37d998d08c73013b4f4190
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-09-30 22:27:54 +0530

    added linked in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,6 +68,7 @@ export default function Home() {
                 <CardHoverEffectDemo />
               </section>
             )}
+            
             <PrizePool />
             <Speakers />
             <section id="aboutUs">
