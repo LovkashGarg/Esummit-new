62000c55d5b81fa3a43881992e92c54db701f77d
LovkashGarg <aggarwallovkesh55@gmail.com>
2024-10-11 00:52:25 +0530

    done

--- src/app/components/speakers.jsx
+++ src/app/components/speakers.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import dynamic from 'next/dynamic';
+
+// Dynamically import 'motion' from framer-motion with SSR disabled
+const MotionDiv = dynamic(() => import('framer-motion').then(mod => mod.motion.div), { ssr: false });
+const MotionImg = dynamic(() => import('framer-motion').then(mod => mod.motion.img), { ssr: false });
 
 const speakers = [
   { name: 'Anoushka Rele', bio: 'Founder and CEO, Polish Me Pretty', image: '/speakers/speaker-2.png' },
@@ -11,9 +15,9 @@ const Speakers = () => {
   return (
     <section className="bg-black flex flex-col items-center pt-10 mb-[10%] md:mb-[0%] min-h-screen">
       <h2 className="text-3xl sm:text-4xl text-white font-bold mb-12">Meet the Speakers</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:gap-3  px-20 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1 md:gap-3 px-20">
         {speakers.map((speaker, index) => (
-          <motion.div
+          <MotionDiv
             key={index}
             className="bg-black p-6 rounded-lg shadow-lg transform transition-transform duration-500 ease-in-out"
             initial={{ opacity: 0, y: 100 }}
@@ -21,7 +25,7 @@ const Speakers = () => {
             transition={{ duration: 0.5, delay: index * 0.3 }}
             viewport={{ once: true }}
           >
-            <motion.img
+            <MotionImg
               src={speaker.image}
               className="h-[200px] w-[200px] md:h-[300px] md:w-[300px] rounded-2xl object-cover"
               width={100}
@@ -31,7 +35,7 @@ const Speakers = () => {
             />
             <h3 className="text-xl font-semibold text-center mt-7">{speaker.name}</h3>
             <p className="text-center text-gray-600">{speaker.bio}</p>
-          </motion.div>
+          </MotionDiv>
         ))}
       </div>
     </section>
